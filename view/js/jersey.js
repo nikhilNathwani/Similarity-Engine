b67bd1f69e0b06aa9d0d5efad370019c8ce7e64c
nikhilNathwani <njn27@cornell.edu>
2015-06-27 18:25:58 -0400

    Got the clothesline working for safari and firefox

--- view/js/jersey.js
+++ view/js/jersey.js
@@ -1,5 +1,100 @@
-
 function createJersey(jerseySVG,x,y,name,number) {
+    //SVG in which the jersey lives
+    jerseySVG.attr("id","outer")
+	            .attr("x",x)
+	            .attr("y",y)
+	            .attr("width",200)
+	            .attr("height",300)
+
+    //Scale everything to the desired amount
+    var g= jerseySVG.append("g")
+                    .attr("transform","scale(1.0)");
+
+    //Drawing the jersey outline           
+    var jerseyShape= g.append("g")
+                        .attr("id","jerseyShape")
+                        .append("path")
+                        .attr("id","outline")
+                        .attr("fill","green")
+                        .attr("transform","translate(-40,0)")
+                        .attr("opacity","0.35")
+                        .attr("stroke","#000000")
+                        .attr("stroke-width","1.5")
+                        .attr("d","M116.462,113.911V39.01c0,0-18.493-5.977-15.317-30.633c0,0-8.033-2.616-8.78-3.363S91.617,9.311,79.29,9.124h-1.305C65.656,9.311,65.656,4.268,64.909,5.015s-8.778,3.363-8.778,3.363C59.305,33.034,40.813,39.01,40.813,39.01v74.901C40.813,113.911,74.434,126.427,116.462,113.911z");
+    scaledText(g,"name",name,24,-40,35,0.9,0.2)
+    scaledText(g,"number",number,64,-40,70,0.9,0.3)
+}
+
+function scaledText(parentGroup,id,value,fontSize,xTrans,yTrans,widthScale,heightScale) {
+    //SVG containing the text (and the viewport for it)            
+    var svgInner= parentGroup.append("svg")
+                          .attr("class","inner")
+                          .attr("id",id+"SVG");
+
+    //Player name
+    svgInner.append("text")
+            .attr("class",id)
+            .attr("id",value)
+            .attr("x",0)
+            .attr("y",0)
+            .attr("font-family","sans-serif")
+            .attr("font-size",fontSize)
+            .attr("text-anchor","left")
+            .attr("dominant-baseline","hanging")
+            .text(value);
+    
+    var tBox= document.getElementById(value).getBBox()
+    var oBox= document.getElementById("outline").getBBox()
+
+    //Set text's SVG to be exactly the size of the text's bounding box
+    svgInner.attr("width", function() {
+        console.log("Text box: w=",tBox.width,"h=",tBox.height,"x=",tBox.x,"y=",tBox.y)
+        return tBox.width * widthScale;
+    })
+    svgInner.attr("height", function() {
+        var tBox= document.getElementById(value).getBBox()
+        return tBox.height;
+    })
+    
+    //Place SVG (i.e. the text() right where I want it on the jersey
+    svgInner.attr("x", function() {
+        console.log("Jersey box: w=",oBox.width,"h=",oBox.height,"x=",oBox.x,"y=",oBox.y)
+        var oX= oBox.x + xTrans;
+        var oW= oBox.width * widthScale;
+        var nW= d3.select(this).attr("width")
+        return oX - (nW-oW)/2 + oBox.width*((1-widthScale)/2);
+    })
+   
+    svgInner.attr("y", function() {
+        var oBox= document.getElementById("outline").getBBox()
+        var oY= oBox.y + yTrans;
+        var oH= oBox.height * heightScale;
+        var nH= d3.select(this).attr("height")
+        console.log(oY - (nH-oH)/2)
+        return oY - (nH-oH)/2;
+    })
+    
+    svgInner.attr("viewBox", function() {
+        var tBox= document.getElementById(value).getBBox()
+        var ttw= tBox.width
+        var tth= tBox.height
+
+        var w= document.getElementById("outline").getBBox().width*widthScale;
+        var h= document.getElementById("outline").getBBox().height*heightScale;
+        
+        var vW= (ttw*ttw)/w;
+        var vH= (tth*tth)/h;
+
+        var vX= (ttw-vW)/2;
+        var vY= (tth-vH)/2;
+
+        return vX + " " + vY + " " + vW + " " + vH;
+    })
+    
+    svgInner.attr("preserveAspectRatio","xMidYMid meet")
+}
+
+function createJersey2(jerseySVG,x,y,name,number) {
 	jerseySVG.attr("x",x)
 			.attr("y",y)
 			.attr("width",200)
@@ -37,7 +132,7 @@ function createJersey(jerseySVG,x,y,name,number) {
 									var pRect= pathNode.getBBox();
 									var textNode = document.getElementById("name");
 									var tRect= textNode.getBBox();
-									console.log(pRect);
+									console.log(tRect,d3.select(this).text());
 									var widthRatio= pRect.width/tRect.width;
 									var heightRatio= (pRect.height/6)/tRect.height;
 									var textScale= Math.min(widthRatio,heightRatio);
